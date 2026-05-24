9033b8fd124ce44fe108a0e0bb6cc4f723583e0f
xych-8866 <356761678@qq.com>
2026-05-24 19:32:30 +0800

    Update create-printful-order.js

--- api/create-printful-order.js
+++ api/create-printful-order.js
@@ -1,10 +1,9 @@
-// /api/create-printful-order.js
-import { sql } from '@vercel/postgres';
+const { sql } = require('@vercel/postgres');
 
 const PRINTFUL_API_URL = 'https://api.printful.com';
 const API_KEY = process.env.PRINTFUL_API_KEY;
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     
@@ -80,4 +79,4 @@ export default async function handler(req, res) {
         console.error('Order error:', error);
         return res.status(500).json({ error: error.message });
     }
-}
+};
